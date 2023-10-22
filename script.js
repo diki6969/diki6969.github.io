82558268a2f059839e874faa7a9b8a0cb92f6d7f
IkyyOfficial <gendonmenjeng@gmail.com>
2023-10-23 01:39:06 +0700

    update script.js

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const video = document.getElementById('video');
-const captureButton = document.getElementById('capture-button');
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+const video = document.getElementById("video");
+const captureButton = document.getElementById("capture-button");
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
 
 // Mengakses kamera depan pengguna
 navigator.mediaDevices
-    .getUserMedia({ video: { facingMode: 'user' } })
-    .then((stream) => {
+    .getUserMedia({ video: { facingMode: "user" } })
+    .then(stream => {
         canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+        canvas.height = video.videoHeight;
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
     })
-    .catch((error) => {
-        console.error('Error accessing the camera: ', error);
+    .catch(error => {
+        console.error("Error accessing the camera: ", error);
     });
 
 // Mengambil gambar dari kamera dan menampilkannya di canvas
-captureButton.addEventListener('click', () => {
+captureButton.addEventListener("click", () => {
     canvas.width = video.videoWidth;
     canvas.height = video.videoHeight;
     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    canvas.style.display = 'block';
-    video.style.display = 'none';
-    captureButton.style.display = 'none';
+    canvas.style.display = "block";
+    video.style.display = "none";
+    captureButton.style.display = "none";
 });
 
 // Optional: Simpan gambar ke file jika diperlukan
